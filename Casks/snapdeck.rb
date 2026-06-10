75945387f0b5ef9963bffa12b1309181634ec171
turbat <b.turbat@mn.data-artist.com>
2026-06-10 12:02:13 +0800

    snapdeck v1.0.0

--- Casks/snapdeck.rb
+++ Casks/snapdeck.rb
@@ -1,8 +1,8 @@
 cask "snapdeck" do
   version "1.0.0"
-  sha256 "PLACEHOLDER"
+  sha256 "45ab18e717a93890b62a528e51af48c0c8ecb02686fef850deb88d191847d80b"
 
-  url "https://github.com/TurbatBay/snapdeck/releases/download/v#{version}/SnapDeck-v#{version}.zip"
+  url "https://github.com/TurbatBay/snapdeck/releases/download/v1.0.0/SnapDeck-v1.0.0.zip"
   name "SnapDeck"
   desc "Open-source macOS screenshot and screen recording tool"
   homepage "https://github.com/TurbatBay/snapdeck"
